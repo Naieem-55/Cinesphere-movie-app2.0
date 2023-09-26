35eded2ac0d4ad348ff3acd1b394e5e69c60bb55
Naieem-55 <naieemislam27@gmail.com>
2023-09-27 01:59:55 +0600

    signUp page added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+import logo from '../assets/logo.png'
+import { useNavigate } from 'react-router-dom'
 
-export default function Header() {
+export default function Header(props) {
+
+  const navigate = useNavigate();
+
   return (
-    <div>Header</div>
+    <Container className='flex a-center j-between'>
+       <div className="logo">
+        <img src={logo} alt='logo' style={{width:'14vw' , height:'12vh'}}></img>
+       </div> 
+       <button onClick={() => navigate(props.login ? "/login" : "/signup")}>
+        {props.login ? "Login" : "Signup"}
+       </button>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  padding: 0 4rem;
+  .logo{
+    img{
+      height:5rem;
+      background:transparent;
+    }
+  }
+  button{
+    padding: 0.5rem 1rem;
+    background-color:#e50914;
+    border: none;
+    cursor:pointer;
+    color: #fff;
+    border-radius:0.3rem;
+    font-weight:bold;
+    font-size:1.05rem;
+  }
+`;
+
+
+
+
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,21 +4,27 @@ import BackgroundImage from '../components/BackgroundImage'
 import Header from '../components/Header'
 
 export default function SignUp() {
+    const [showPassword , setShowPassword] = React.useState(false);
   return (
-    <Container>
+    <Container showPassword={showPassword}>
         <BackgroundImage />
         <div className="content">
-        <Header/>
+        <Header login/>
         <div className="body flex column a-center j-center">
             <div className="text flex column">
                 <h1>Unlimited Movies , TV shows and more..</h1>
-                <h4>Watch anyWhere . Cancel anyTime.</h4>
-                <h6>Ready to Watch ? Enter your Email to create or restart memberShip</h6>
+                <h3>Watch anyWhere . Cancel anyTime.</h3>
+                <h4>Ready to Watch ? Enter your Email to create or restart memberShip !</h4>
             </div>
             <div className="form">
                 <input type="email" placeholder="Email Address" name='email'/>
-                <input type="password" placeholder='password' name='password'/>
-                <button>Get Started</button>
+                {
+                    showPassword && (<input type="password" placeholder='password' name='password'/>)
+                }
+
+                {
+                    !showPassword && <button onClick={() => setShowPassword(true)}>Get Started</button>
+                }
             </div>
             <button>Log In</button>
         </div>               
@@ -38,5 +44,50 @@ const Container = styled.div`
         width: 100vw;
         display: grid;
         grid-template-rows: 15vh 85vh;
+    }
+    .body{
+        gap:1rem;
+        text{
+            gap:1rem;
+            text-align: center;
+            font-size: 2rem;
+            h1{
+                padding:0 25rem;
+            }
+        }
+        .form{
+            display: grid;
+            grid-template-columns: ${({showPassword}) => showPassword ? '1fr 1fr' : '2fr 1fr'};
+            width:60%;
+            input{
+                color: black;
+                border: none;
+                padding:1.5rem;
+                font-size: 1rem;
+                border: 1px solid black;
+                &:focus{
+                    outline: none;
+                }
+            }
+            button{
+                padding: 0.5rem 1rem;
+                background-color:#e50914;
+                border: none;
+                cursor:pointer;
+                color: #fff;
+                font-weight:bold;
+                font-size:1.05rem;
+            }
+        }
+        button{
+            padding: 0.5rem 1rem;
+            background-color:#e50914;
+            border: none;
+            cursor:pointer;
+            color: #fff;
+            border-radius:0.3rem;
+            font-weight:bold;
+            font-size:1.05rem;
+        }
     }
 `;
